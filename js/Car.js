d545b530863107dc1e6bc5445fb4e6f185f1d176
Steve <rkglimmer16@gmail.com>
2021-04-20 22:34:26 -0300

    Basic preliminary setup of Car, Maze and Wall class

---
+++ js/Car.js
@@ -0,0 +1,22 @@
+class Car extends GameObject {
+  this.startPosition = createVector(0,0);
+
+  this.maxSpeed = 0; // set up later
+
+  constructor(pos) {
+    this.position = pos.copy();
+    this.startPosition = pos.copy();
+
+    this.sprite.spriteType = SpriteType.Rect;
+    this.collider = new RectCollider();
+  }
+
+  this.setSize = function(width, height) {
+    this.collider.size.width = width;
+    this.collider.size.height = height;
+
+    this.sprite.size.width = width;
+    this.sprite.size.height = height;
+  }
+
+}
---
+++ js/Wall.js
@@ -0,0 +1,15 @@
+class Wall extends GameObject{
+  constructor(pos) {
+    this.position = pos.copy();
+    this.sprite.spriteType = SpriteType.Rect;
+    this.collider = new RectCollider();
+  }
+
+  this.setSize = function(width, height) {
+    this.collider.size.width = width;
+    this.collider.size.height = height;
+
+    this.sprite.size.width = width;
+    this.sprite.size.height = height;
+  }
+}
