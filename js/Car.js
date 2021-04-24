c5640fbf966f631d68ae5e5d0c849b4fe66881d9
Steve <rkglimmer16@gmail.com>
2021-04-24 10:52:22 -0300

    Add update method for gameObjects. Implemented

--- js/Car.js
+++ js/Car.js
@@ -29,4 +29,39 @@ class Car extends GameObject {
     this.rotation = rotation;
   }
 
+  // public interface
+  update = function(deltaTime) {
+    let motionProperty = {speed:5, direction:createVector(-1,-2)};
+
+    this.move(motionProperty.speed, motionProperty.direction, deltaTime);
+
+    // generation output data for the change
+    let updateProperty = {motionProperty:motionProperty};
+
+    return updateProperty;
+  }
+
+  // v is a float, direction is a vector
+  move = function(v, rawDirection, deltaTime) {
+    let rawDirMagnitude = Math.sqrt(Math.pow(rawDirection.x,2) + Math.pow(rawDirection.y,2));
+
+    let direction = p5.Vector.mult(rawDirection, 1/rawDirMagnitude);
+
+
+
+    // make sure speed does not exceed maximum speed
+    if (v > this.maxSpeed) {
+      v = this.maxSpeed;
+    }
+    else if (v < -this.maxSpeed) {
+      v = -this.maxSpeed;
+    }
+
+    let moveVector = p5.Vector.mult(direction, v*deltaTime);
+
+    this.moveBy(moveVector);
+
+    return {speed:v, direction:{x:direction.x, y:direction.y}}
+  }
+
 }
