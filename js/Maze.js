a7c09c76e98ba86d4920926353a8b97dc1d2ea3c
Steve <rkglimmer16@gmail.com>
2021-04-23 21:32:01 -0300

    my

--- js/Maze.js
+++ js/Maze.js
@@ -10,11 +10,13 @@ class Maze {
     let wall2 = new Wall(createVector(100,50), 100, 20, Math.PI/2);
     let wall3 = new Wall(createVector(250,350), 100, 20);
     let wall4 = new Wall(createVector(300,300), 100, 20, -Math.PI/4);
+    let wall5 = new Wall(createVector(150,150), 100, 20, -Math.PI/4);
 
     this.maze.push(wall1);
     this.maze.push(wall2);
     this.maze.push(wall3);
     this.maze.push(wall4);
+    this.maze.push(wall5);
 
 
   }
